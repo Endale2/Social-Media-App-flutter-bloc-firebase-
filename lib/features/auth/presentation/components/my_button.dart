4f7bcf25718e684ecdb2fe8b923fd3d5705f2776
Endale2 <endale406@gmail.com>
2025-02-05 12:45:08 +0300

    login page updated

---
+++ lib/features/auth/presentation/components/my_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+
+class MyButton extends StatelessWidget {
+  final void Function()? onTap;
+  final String text;
+  const MyButton({super.key, required this.onTap, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          child: Center(
+        child: Text(text),
+      )),
+    );
+  }
+}
--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:socialx/features/auth/presentation/components/my_button.dart';
+import 'package:socialx/features/auth/presentation/components/my_text_field.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,24 +10,49 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
       child: Center(
-        child: Column(
-          children: [
-            //icon
-            Icon(Icons.lock_open_rounded,
-                size: 80, color: Theme.of(context).colorScheme.primary)
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            children: [
+              //icon
+              Icon(Icons.lock_open_rounded,
+                  size: 80, color: Theme.of(context).colorScheme.primary),
 
-            //welcome back message
+              const SizedBox(height: 50),
 
-            //email
-            //password
-            //login button
-            //not registered yet?
-          ],
+              //welcome back message
+              Text(
+                "Welcome back we missed you!",
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary, fontSize: 16),
+              ),
+
+              const SizedBox(height: 25),
+              //email
+              MyTextField(
+                  controller: emailController,
+                  hintText: "email",
+                  obscureText: false),
+
+              const SizedBox(height: 10),
+              //password
+              MyTextField(
+                  controller: passwordController,
+                  hintText: "password",
+                  obscureText: true),
+              //login button
+              const SizedBox(height: 25),
+              MyButton(onTap: () {}, text: "Login")
+              //not registered yet?
+            ],
+          ),
         ),
       ),
     ));
