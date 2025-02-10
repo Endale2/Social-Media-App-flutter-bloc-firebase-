bd1db6b690a636d306daebadf4976a15d7479f42
Endale2 <endale406@gmail.com>
2025-02-10 11:47:55 +0300

    updated

--- lib/features/post/presentation/components/comment_tile.dart
+++ lib/features/post/presentation/components/comment_tile.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:socialx/features/auth/domain/entities/app_user.dart';
 import 'package:socialx/features/post/domain/entities/comment.dart';
 
-class CommentTile extends StatelessWidget {
+class CommentTile extends StatefulWidget {
   final Comment comment;
   const CommentTile({super.key, required this.comment});
 
+  @override
+  State<CommentTile> createState() => _CommentTileState();
+}
+
+class _CommentTileState extends State<CommentTile> {
+  AppUser? currentUser;
+  bool isOwnPost = false;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -12,12 +20,13 @@ class CommentTile extends StatelessWidget {
       child: Row(
         children: [
           //name
-          Text(comment.userName, style: TextStyle(fontWeight: FontWeight.bold)),
+          Text(widget.comment.userName,
+              style: TextStyle(fontWeight: FontWeight.bold)),
           const SizedBox(width: 10),
 
           //comment text
 
-          Text(comment.text)
+          Text(widget.comment.text)
 
           //button to delete comment
         ],
