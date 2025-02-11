bf4c432511983a23063b1c2d7ca70e9b8308091b
Endale2 <endale406@gmail.com>
2025-02-11 11:07:36 +0300

    updated

---
+++ lib/features/search/presentation/cubits/search_state.dart
@@ -0,0 +1,23 @@
+import 'package:socialx/features/profile/domain/entities/profile_user.dart';
+
+abstract class SearchState {}
+
+//initial
+
+class SearchInitial extends SearchState {}
+
+//loading
+class SearchLoading extends SearchState {}
+
+//loaded
+class SearchLoaded extends SearchState {
+  final List<ProfileUser?> users;
+  SearchLoaded(this.users);
+}
+
+//error
+
+class SearchError extends SearchState {
+  final String message;
+  SearchError(this.message);
+}
