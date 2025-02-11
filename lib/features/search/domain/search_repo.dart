bf2e24ee8a47a1020e86a13105053fe28dcd8077
Endale2 <endale406@gmail.com>
2025-02-11 11:01:21 +0300

    updated

--- lib/features/search/domain/search_repo.dart
+++ lib/features/search/domain/search_repo.dart
@@ -1,1 +1,5 @@
-abstract class SearchRepo {}
+import 'package:socialx/features/profile/domain/entities/profile_user.dart';
+
+abstract class SearchRepo {
+  Future<List<ProfileUser?>> searchUsers(String query);
+}
